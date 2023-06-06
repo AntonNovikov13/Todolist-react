3ad1335a82d78a178c187fcffebdc15f73af85b7
AntonNovikov13 <antonnovikov13@mail.ru>
2023-06-06 18:38:17 +0300

    fix services

--- src/tasks/tasks.services.js
+++ src/tasks/tasks.services.js
@@ -1,47 +1,43 @@
 const baseUrl = 'https://63b875bc6f4d5660c6d7280e.mockapi.io/api/v1/tasks';
 
-export const createTask = (taskData) => {
-    return fetch(baseUrl, {
+export const createTask = async (taskData) => {
+    const response = await fetch(baseUrl, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json;charset=utf-8',
         },
         body: JSON.stringify(taskData),
-    }).then((response) => {
-        if (!response.ok) {
-            throw new Error('Failed to create task');
-        }
     });
+    if (!response.ok) {
+        throw new Error('Failed to create task');
+    }
 };
 
-export const updateTask = (taskId, taskData) => {
-    return fetch(`${baseUrl}/${taskId}`, {
+export const updateTask = async (taskId, taskData) => {
+    const response = await fetch(`${baseUrl}/${taskId}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json;charset=utf-8',
         },
         body: JSON.stringify(taskData),
-    }).then((response) => {
-        if (!response.ok) {
-            throw new Error('Failed to update task');
-        }
     });
+    if (!response.ok) {
+        throw new Error('Failed to update task');
+    }
 };
 
-export const deleteTask = (taskId) => {
-    return fetch(`${baseUrl}/${taskId}`, {
+export const deleteTask = async (taskId) => {
+    const response = await fetch(`${baseUrl}/${taskId}`, {
         method: 'DELETE',
-    }).then((response) => {
-        if (!response.ok) {
-            throw new Error('Failed to delete task');
-        }
     });
+    if (!response.ok) {
+        throw new Error('Failed to delete task');
+    }
 };
 
-export const fetchTasksList = () => {
-    return fetch(baseUrl).then((response) => {
-        if (response.ok) {
-            return response.json();
-        }
-    });
+export const fetchTasksList = async () => {
+    const response = await fetch(baseUrl);
+    if (response.ok) {
+        return response.json();
+    }
 };
